035c168fae5e36fe4f89f77a4800be22caa9348a
SandroMaglione <lass.maglio@gmail.com>
2024-07-10 16:55:02 +0200

    custom error type

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import { Effect } from "effect";
 
-const fetchRequest = Effect.tryPromise(() =>
-  fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/")
-);
+interface FetchError {
+  readonly _tag: "FetchError";
+}
+
+interface JsonError {
+  readonly _tag: "JsonError";
+}
+
+const fetchRequest = Effect.tryPromise({
+  try: () => fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/"),
+  catch: (): FetchError => ({ _tag: "FetchError" }),
+});
 
 const jsonResponse = (response: Response) =>
-  Effect.tryPromise(() => response.json());
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: (): JsonError => ({ _tag: "JsonError" }),
+  });
 
 const main = fetchRequest.pipe(
   Effect.flatMap(jsonResponse),
-  Effect.catchTag("UnknownException", () =>
-    Effect.succeed("There was an error")
-  )
+  Effect.catchTag("FetchError", () => Effect.succeed("Fetch error")),
+  Effect.catchTag("JsonError", () => Effect.succeed("Json error"))
 );
 
 Effect.runPromise(main).then(console.log);
