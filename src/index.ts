5c7e877b05343021e94d27057e8d3549c67bf508
SandroMaglione <lass.maglio@gmail.com>
2024-07-10 16:27:11 +0200

    `catchTag`

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import { Effect } from "effect";
 
 const fetchRequest = Effect.tryPromise(() =>
-  fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
+  fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/")
 );
 
 const jsonResponse = (response: Response) =>
   Effect.tryPromise(() => response.json());
 
-const savePokemon = (pokemon: unknown) =>
-  Effect.tryPromise(() =>
-    fetch("/api/pokemon", { body: JSON.stringify(pokemon) })
-  );
-
 const main = fetchRequest.pipe(
   Effect.flatMap(jsonResponse),
-  Effect.flatMap(savePokemon)
+  Effect.catchTag("UnknownException", () =>
+    Effect.succeed("There was an error")
+  )
 );
 
-Effect.runPromise(main);
+Effect.runPromise(main).then(console.log);
