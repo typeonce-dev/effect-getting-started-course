662b88d84d7448756f5f5ba81ac616a940d7b8be
SandroMaglione <lass.maglio@gmail.com>
2024-07-10 11:58:04 +0200

    `Effect.promise`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { Console, Effect } from "effect";
+import { Effect } from "effect";
 
-const main = Console.log("Hello world");
+const fetchRequest = Effect.promise(() =>
+  fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
+);
 
-Effect.runSync(main);
+const jsonResponse = (response: Response) =>
+  Effect.promise(() => response.json());
+
+const main = Effect.flatMap(fetchRequest, jsonResponse);
+
+Effect.runPromise(main);
