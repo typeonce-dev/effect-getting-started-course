f9de9518a58841573aaa79f6569ad270df7fb5c4
SandroMaglione <lass.maglio@gmail.com>
2024-08-23 15:48:00 +0200

    no need of `.of`

--- src/PokeApi.ts
+++ src/PokeApi.ts
@@ -40,26 +40,23 @@ export class PokeApi extends Context.Tag("PokeApi")<
     Layer.provide(Layer.mergeAll(PokemonCollection.Live, BuildPokeApiUrl.Live))
   );
 
-  static readonly Test = Layer.succeed(
-    this,
-    PokeApi.of({
-      getPokemon: Effect.gen(function* () {
-        const response = yield* Effect.tryPromise({
-          try: () => fetch(`http://localhost:3000/api/v2/pokemon/garchomp/`),
-          catch: () => new FetchError(),
-        });
+  static readonly Test = Layer.succeed(this, {
+    getPokemon: Effect.gen(function* () {
+      const response = yield* Effect.tryPromise({
+        try: () => fetch(`http://localhost:3000/api/v2/pokemon/garchomp/`),
+        catch: () => new FetchError(),
+      });
 
-        if (!response.ok) {
-          return yield* new FetchError();
-        }
+      if (!response.ok) {
+        return yield* new FetchError();
+      }
 
-        const json = yield* Effect.tryPromise({
-          try: () => response.json(),
-          catch: () => new JsonError(),
-        });
+      const json = yield* Effect.tryPromise({
+        try: () => response.json(),
+        catch: () => new JsonError(),
+      });
 
-        return yield* Schema.decodeUnknown(Pokemon)(json);
-      }),
-    })
-  );
+      return yield* Schema.decodeUnknown(Pokemon)(json);
+    }),
+  });
 }
