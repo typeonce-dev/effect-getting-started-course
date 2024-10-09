b749f915ad561dab9bdacc11d547c5dc91b978fe
SandroMaglione <lass.maglio@gmail.com>
2024-10-09 18:07:54 +0200

    refactor platform request example

--- src/+1/platform.ts
+++ src/+1/platform.ts
@@ -1,14 +1,25 @@
 import {
   FetchHttpClient,
   HttpClient,
+  HttpClientRequest,
   HttpClientResponse,
 } from "@effect/platform";
-import { Effect } from "effect";
+import { Effect, flow } from "effect";
 import { Pokemon } from "../schemas";
 
-export const main = HttpClient.get(
-  "https://pokeapi.co/api/v2/pokemon/squirtle"
-).pipe(
+export const main = Effect.gen(function* () {
+  const baseClient = yield* HttpClient.HttpClient;
+  const pokeApiClient = baseClient.pipe(
+    HttpClient.mapRequest(
+      flow(
+        HttpClientRequest.acceptJson,
+        HttpClientRequest.prependUrl("https://pokeapi.co/api/v2")
+      )
+    )
+  );
+
+  return yield* pokeApiClient.get("/pokemon/squirtle");
+}).pipe(
   Effect.flatMap(HttpClientResponse.schemaBodyJson(Pokemon)),
   Effect.scoped,
   Effect.provide(FetchHttpClient.layer)
